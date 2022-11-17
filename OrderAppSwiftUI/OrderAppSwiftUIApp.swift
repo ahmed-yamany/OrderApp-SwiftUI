44fbdee94c2e9670c67e371c14c7214b249d4d92
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2022-11-17 10:47:47 +0200

    Initial Commit

---
+++ OrderAppSwiftUI/OrderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppSwiftUIApp.swift
+//  OrderAppSwiftUI
+//
+//  Created by Ahmed Yamany on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
